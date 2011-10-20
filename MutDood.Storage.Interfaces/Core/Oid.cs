e6493219a2a278d3186189cab0073bca3b0f0cef
Michal Jasiorowski <michajas@gmail.com>
2011-10-20 20:58:07 +0200

    Kolejne

---
+++ MutDood.Storage.Interfaces/Core/Oid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MutDood.Storage.Interfaces.Core
+{
+    public class Oid
+    {
+        public byte[] Oli { get; set; }
+        public byte[] Uid { get; set; }
+    }
+}
---
+++ MutDood.Storage.Interfaces/IStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MutDood.Storage.Interfaces.Core;
+
+namespace MutDood.Storage.Interfaces
+{
+    public interface IStorage
+    {
+        Oid Save(IStorable toStore);
+
+    }
+}
